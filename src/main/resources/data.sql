ed9be30d510646d1632fd3036ad941e9a133dca5
Bilal A <bila0765@stud.kea.dk>
2021-06-10 12:02:41 +0200

    first

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE kommune (
+                         id long auto_increment primary key,
+                         kommunekode long not null,
+                         navn varchar(250) not null
+);
+
+CREATE TABLE sogne(
+                      id long auto_increment primary key,
+                      sognekode long not null,
+                      navn varchar(250) not null,
+                      kommune varchar(250) not null,
+                      smittetryk long not null,
+                      nedlukningsdato date not null
+);
